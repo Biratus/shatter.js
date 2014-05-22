32d7e9c4e1e0615ce5390419afabcbfaed3188fd
Cory Gugler <cory.gugler@gmail.com>
2014-05-22 00:06:29 -0400

    Update examples from dev

--- examples/js/dom.js
+++ examples/js/dom.js
@@ -1,13 +1,18 @@
-var image = new Image();
+var image = new Image(),
+    shattered = [],
+    YLOC = 75;
+
 image.src = "img/BlueMarbleNasa.png";
+
 // jGravity seems to disable input even though ignored
-// a jq click seems to fix it
+// a simulated click seems to fix it
 $('#numPieces').click();
 
 // create shatter object after image has loaded
 image.addEventListener("load", function() {
     var div = document.querySelectorAll('.shatter');
     var shatter = new Shatter(image, 10);
+    shattered.push(shatter);
 
     // loop through images in shatter object and insert into 
     // dom at correct position
@@ -29,24 +34,59 @@ image.addEventListener("load", function() {
         var numPieces = $('#numPieces').val();
         // 10 pieces if none specified
         var shatter = new Shatter(image, numPieces || 10);
+        shattered.push(shatter);
 
         placeShatter(shatter, div[0]);
+        // place debug
+        if (document.getElementsByClassName('debug')[0].checked) {
+            var center = (window.innerWidth / 2) - image.width / 2;
+            placeImageAbsolute(shatter.debug, div[0], center, YLOC);
+            shattered.push(shatter.debug);
+        }
+
         // clear out the input, otherwise the input box becomes unresponsive
         $('#numPieces').val('');
 
-    // TODO
-    // figure out why it freaks out on first new shatter without a short delay before calling jGravity.
-    window.setTimeout(function() {
-        $('.container').jGravity({
-            target: '.shatter img',
-            ignoreClass: 'ignore',
-            weight: 25,
-            depth: 5,
-            drag: true
+        if (!document.getElementsByClassName('shatter-check')[0].checked) {
+            // TODO
+            // figure out why it freaks out on first new shatter without a short delay before calling jGravity.
+            window.setTimeout(function() {
+                $('.container').jGravity({
+                    target: '.shatter img',
+                    ignoreClass: 'ignore',
+                    weight: 25,
+                    depth: 5,
+                    drag: true
+                });
+            }, 100);
+        }
+        $('.shatter img').click(function() {
+            $('.container').jGravity({
+                target: '.shatter img',
+                ignoreClass: 'ignore',
+                weight: 25,
+                depth: 5,
+                drag: true
+            });
+            $('.shatter img').off('click');
         });
-    }, 100);
     });
 }, false);
+
+// clear out all shattered images
+$('.remove-all').on('click', function() {
+    shattered.forEach(function(shatter) {
+        if (shatter.src) {
+            shatter.remove();
+        } else {
+            shatter.images.forEach(function(image) {
+                // box2d remove method
+                image.image.remove();
+            });
+        }
+    });
+    shattered = [];
+});
 
 /**
  * Places given shatter objects images into the specified dom element
@@ -55,12 +95,33 @@ image.addEventListener("load", function() {
  * @param {object} domElement - The dom element to append images to
  */
 function placeShatter (shatter, domElement) {
-        // adjustment to center image on screen
-        var adjustment = (window.innerWidth / 2) - image.width / 2;
-        for (var i = 0; i < shatter.images.length; i++) {
-            shatter.images[i].image.style.position = 'absolute';
-            shatter.images[i].image.style.left = shatter.images[i].x + adjustment + 'px';
-            shatter.images[i].image.style.top = shatter.images[i].y + 75 + 'px';
-            domElement.appendChild(shatter.images[i].image);
-        }
+    // adjustment to center image on screen
+    var adjustment = (window.innerWidth / 2) - image.width / 2;
+    for (var i = 0; i < shatter.images.length; i++) {
+        placeImageAbsolute(shatter.images[i].image,
+                           domElement, 
+                           shatter.images[i].x, 
+                           shatter.images[i].y,
+                           adjustment,
+                           YLOC);
+    }
+}
+
+/**
+ * Places an image into dom element and absolutely positions
+ *
+ * @param {object} image - image to append to dom
+ * @param {object} domElement - dom element to append image 
+ * @param {number} x - x coordinate to position image at
+ * @param {number} y - y coordinate to position image at
+ * @param {number} xOff - x offset added to x coordinate
+ * @param {number} yOff - y offset added to x coordinate
+ */
+function placeImageAbsolute (image, domElement, x, y, xOff, yOff) {
+    var xOff = xOff || 0;
+    var yOff = yOff || 0;
+    image.style.position = 'absolute';
+    image.style.left = x + xOff + 'px';
+    image.style.top = y + yOff + 'px';
+    domElement.appendChild(image);
 }
